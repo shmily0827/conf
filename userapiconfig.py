0f2528b9f562e6918df087ec8d261959f0e75491
shmily0827 <shmily@email.cufe.edu.cn>
2017-03-13 16:59:33 +0800

    Update userapiconfig.py

--- userapiconfig.py
+++ userapiconfig.py
@@ -14,7 +14,7 @@ MU_REGEX = '%5m%id.%suffix'
 SERVER_PUB_ADDR = '127.0.0.1'  # mujson_mgr need this to generate ssr link
 API_INTERFACE = 'modwebapi'  # glzjinmod, modwebapi
 
-WEBAPI_URL = 'https://api.kantianxia.win'
+WEBAPI_URL = ''
 WEBAPI_TOKEN = '4hrZTJX2Dr64rhHqBAtDGDNdGZjNV7p8'
 
 # mudb
