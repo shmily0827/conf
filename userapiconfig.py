3b899ad66caab410422c721d2fa092c4166348b3
shmily0827 <shmily@email.cufe.edu.cn>
2017-03-13 16:51:35 +0800

    Add files via upload

--- userapiconfig.py
+++ userapiconfig.py
@@ -1,31 +1,31 @@
 ﻿# Config
-NODE_ID = 18
+NODE_ID = 20
 
 
-
-#hour,set 0 to disable
+# hour,set 0 to disable
 SPEEDTEST = 6
 CLOUDSAFE = 1
 ANTISSATTACK = 0
 AUTOEXEC = 0
-MULTI_THREAD = 0
 
 MU_SUFFIX = 'zhaoj.in'
 MU_REGEX = '%5m%id.%suffix'
 
-SERVER_PUB_ADDR = '127.0.0.1' # mujson_mgr need this to generate ssr link
-API_INTERFACE = 'glzjinmod' #mudbjson, sspanelv2, sspanelv3, sspanelv3ssr, muapiv2(not support)
+SERVER_PUB_ADDR = '127.0.0.1'  # mujson_mgr need this to generate ssr link
+API_INTERFACE = 'modwebapi'  # glzjinmod, modwebapi
 
-#mudb
+WEBAPI_URL = 'https://api.kantianxia.win'
+WEBAPI_TOKEN = '4hrZTJX2Dr64rhHqBAtDGDNdGZjNV7p8'
+
+# mudb
 MUDB_FILE = 'mudb.json'
 
 # Mysql
-MYSQL_HOST = ''
+MYSQL_HOST = '127.0.0.1'
 MYSQL_PORT = 3306
-MYSQL_USER = 'sspanel'
-MYSQL_PASS = ''
-MYSQL_DB = 'sspanel'
-MYSQL_UPDATE_TIME = 60
+MYSQL_USER = 'ss'
+MYSQL_PASS = 'ss'
+MYSQL_DB = 'shadowsocks'
 
 MYSQL_SSL_ENABLE = 0
 MYSQL_SSL_CA = ''
@@ -41,10 +41,10 @@ API_UPDATE_TIME = 60
 
 # Manager (ignore this)
 MANAGE_PASS = 'ss233333333'
-#if you want manage in other server you should set this value to global ip
+# if you want manage in other server you should set this value to global ip
 MANAGE_BIND_IP = '127.0.0.1'
-#make sure this port is idle
+# make sure this port is idle
 MANAGE_PORT = 23333
-#Safety
 
+# Safety
 IP_MD5_SALT = 'randomforsafety'
