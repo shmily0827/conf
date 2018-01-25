cbd658f5cfc86f610c5f8eab40a08b289fd0eec1
shmily0827 <shmily@email.cufe.edu.cn>
2018-01-25 15:18:36 +0800

    Update one.sh

--- one.sh
+++ one.sh
@@ -1,21 +1,12 @@
 #!/bin/sh  
 echo 安装依赖
 cd
+yum -y wget
 yum -y install python-setuptools && easy_install pip
-
 yum -y install git
-
 yum -y groupinstall "Development Tools"
 
-wget https://github.com/jedisct1/libsodium/releases/download/1.0.10/libsodium-1.0.10.tar.gz
 
-tar xf libsodium-1.0.10.tar.gz && cd libsodium-1.0.10
-
-./configure && make -j2 && make install
-
-echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
-
-ldconfig
 echo 下载ssr源码
 cd 
 git clone -b manyuser https://github.com/glzjin/shadowsocks.git
@@ -30,23 +21,13 @@ pip install cymysql
 
 pip install -r requirements.txt
 
-cp config.json user-config.json
+wget https://raw.githubusercontent.com/shmily0827/conf-/master/user-config.json
 
 wget https://raw.githubusercontent.com/shmily0827/conf-/master/userapiconfig.py
 
-echo  安装守护进程
-cd
-yum install supervisor python-pip -y
-
-pip install supervisor==3.1
-
-chkconfig supervisord on
-
-wget https://raw.githubusercontent.com/shmily0827/conf-/master/supervisord.conf -O /etc/supervisord.conf
-
-wget https://raw.githubusercontent.com/shmily0827/conf-/master/supervisord -O /etc/init.d/supervisord
-chmod +x /etc/init.d/supervisord
-service supervisord start
+echo "请输入你的节点ID:"
+read nodeid
+sed -i '/NODE_ID/d' userapiconfig.py
 
 echo 优化连接参数
 echo "  * soft nofile 51200
@@ -85,4 +66,41 @@ net.ipv4.tcp_rmem = 4096 87380 67108864
 net.ipv4.tcp_wmem = 4096 65536 67108864
 
 net.ipv4.tcp_mtu_probing = 1 ">> /etc/sysctl.conf
+
 sysctl -p
+
+
+echo ' #########开始安装supervisiord#########'
+
+wget –no-check-certificate https://pypi.python.org/packages/source/s/supervisor/supervisor-3.0.tar.gz
+tar -zxvf supervisor-3.0.tar.gz && cd supervisor-3.0
+python setup.py install
+echo_supervisord_conf > /etc/supervisord.conf
+sed -i '$a [program:shadowsocks]' /etc/supervisord.conf
+sed -i '$a command = python server.py' /etc/supervisord.conf
+sed -i '$a directory = /root/shadowsocks' /etc/supervisord.conf
+sed -i '$a user=root' /etc/supervisord.conf
+sed -i '$a autostart=true' /etc/supervisord.conf
+sed -i '$a autorestart=true' /etc/supervisord.conf
+sed -i '$a stderr_logfile = /var/log/shadowsocks.log' /etc/supervisord.conf
+sed -i '$a stdout_logfile = /var/log/shadowsocks.log' /etc/supervisord.conf
+sed -i '$a startsecs=3' /etc/supervisord.conf
+/usr/bin/supervisord -c /etc/supervisord.conf
+supervisorctl reload 
+sed -i '$a\supervisord' /etc/rc.d/rc.local
+fi
+
+echo '如果你是Centos7，请按<Y>继续设置开机启动,否则按其他键退出:'
+read c7
+if [ "$c7" == "Y" ] ; then
+cd /root
+echo "#!/bin/sh" >supervisord.sh
+sed -i '$a #chkconfig: 2345 80 80' supervisord.sh
+sed -i '$a #description: auto start supervisord' supervisord.sh
+sed -i '$a supervisord' supervisord.sh
+chmod +x supervisord.sh
+mv supervisord.sh /etc/init.d/ 
+chkconfig --add supervisord.sh
+chkconfig --list supervisord.sh
+fi
+echo "配置完成，Enjoy it！"
